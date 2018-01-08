e6d14269555835d775ef588d78b3ed8da429f2ee
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-01-08 12:38:19 -0600

    Added Initial Entries Function
    Added async function and test function for initial entries.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,6 +75,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -82,11 +83,20 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+           loadFeed(0, done);
+         });
 
+         it('a single .entry element within the .feed container', function(){
+           expect($('.feed .entry').length).toBeGreaterThan(0);
+         });
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('News Feed Selection', function(){
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
